04963fb01c51ad75ce4b2def93cadef462d3c427
riregot2545 <34894728+riregot2545@users.noreply.github.com>
2019-10-19 15:20:41 +0300

    10 times bigger hello world!

--- src/com/nix/Main.java
+++ src/com/nix/Main.java
@@ -2,6 +2,8 @@ package com.nix;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int i=0;i<10;i++) {
+            System.out.println("Hello world!");
+        }
     }
 }
