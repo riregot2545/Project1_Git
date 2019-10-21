3096451ef114aa9779b8b44ee48ad9bb17733931
unknown <dima4elom@gmail.com>
2019-10-21 12:29:30 +0300

    Add new i

--- src/com/nix/Main.java
+++ src/com/nix/Main.java
@@ -3,7 +3,7 @@ package com.nix;
 public class Main {
     public static void main(String[] args) {
         for (int i=0;i<10;i++) {
-            System.out.println("Hello world!");
+            System.out.println("Hello world "+i);
         }
     }
 }
